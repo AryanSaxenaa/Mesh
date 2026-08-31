c8330619f32c348ab11811bad68fc47967c1bd3d
AryanSaxenaa <aryansaxenaalig@gmail.com>
2026-08-31 11:08:00 +0530

    Fuzz sync frame decoders

---
+++ sync_fuzz_test.go
@@ -0,0 +1,33 @@
+package mesh0
+
+import (
+	"bufio"
+	"bytes"
+	"crypto/ed25519"
+	"testing"
+)
+
+func FuzzSyncDecodersNeverPanic(f *testing.F) {
+	f.Add([]byte("M0HL"))
+	f.Add(encodeActorRanges(nil))
+	f.Add(encodeDigest(VersionVector{}, [32]byte{}))
+	f.Add([]byte{1})
+	f.Fuzz(func(t *testing.T, input []byte) {
+		_, _ = decodeHello(input)
+		_, _ = decodeActorRanges(input)
+		_, _, _ = decodeDigest(input)
+		_ = decodeWireError(input)
+		_, _, _ = readWireFrame(bufio.NewReader(bytes.NewReader(input)))
+		_, _ = decodeSignedBatch(input, make(ed25519.PublicKey, ed25519.PublicKeySize), DatabaseID{})
+	})
+}
+
+func FuzzEqualityIndexValueKeysNeverPanic(f *testing.F) {
+	f.Add(byte(0), []byte(nil))
+	f.Add(byte(StringValue), []byte("value"))
+	f.Fuzz(func(t *testing.T, kind byte, raw []byte) {
+		value := Value{Kind: ValueKind(kind), Bytes: append([]byte(nil), raw...)}
+		_, _ = equalityValueKey(value)
+		_ = validateEqualityIndex(EqualityIndex{Collection: string(raw), Path: string(raw)})
+	})
+}
